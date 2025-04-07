264a67b5b4c5ce8540cdd94052bc1119f087d132
mvxconrad <mvxconrad@gmail.com>
2025-04-07 19:26:38 +0000

    changed dashboard route

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,13 +19,13 @@ app.add_middleware(
 
 # Include routers
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
-app.include_router(study_sets.router, prefix="/study_sets", tags=["Study Sets"])
-app.include_router(files.router, prefix="/files", tags=["File Management"])
-app.include_router(dashboard.router, prefix="", tags=["Dashboard"]) 
-app.include_router(upload.router, tags=["File Upload"])
-app.include_router(gpt.router)
+app.include_router(users.router, prefix="/api/users", tags=["Users"])
+app.include_router(dashboard.router, prefix="/api/dashboard", tags=["Dashboard"])
+app.include_router(study_sets.router, prefix="/api/study_sets", tags=["Study Sets"])
+app.include_router(files.router, prefix="/api/files", tags=["File Management"])
+app.include_router(upload.router, prefix="/api/upload", tags=["File Upload"])
+app.include_router(gpt.router, prefix="/api/gpt", tags=["AI"])
+
 
 @app.get("/")
 def root():
