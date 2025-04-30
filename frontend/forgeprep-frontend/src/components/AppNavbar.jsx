b20f8e5be15ec43f293dc0ead956f64133f2be2d
mvxconrad <mvxconrad@gmail.com>
2025-04-30 13:48:16 +0000

    fixing merge conflicts

--- frontend/forgeprep-frontend/src/components/AppNavbar.jsx
+++ frontend/forgeprep-frontend/src/components/AppNavbar.jsx
@@ -89,7 +89,7 @@ const AppNavbar = () => {
                   </Nav.Link>
 
                   {/* Conditionally Render Admin Dashboard Link */}
-                  {user.is_admin && user.is_verified && (
+                  {user.is_admin && (
                     <Nav.Link as={Link} to="/admin" active={isActive("/admin")}>
                       Admin Dashboard
                     </Nav.Link>
--- frontend/forgeprep-frontend/src/components/ProtectedRoute.jsx
+++ frontend/forgeprep-frontend/src/components/ProtectedRoute.jsx
@@ -15,10 +15,11 @@ const ProtectedRoute = ({ children }) => {
 
   if (!user) return <Navigate to="/login" replace />;
 
-  // ✅ Redirect unverified users to verification prompt
-  if (!user.is_verified) return <Navigate to="/verify-email-prompt" replace />;
+  // ✅ Redirect unverified users to verification prompt. Commented out for now.
+  // if (!user.is_verified) return <Navigate to="/verify-email-prompt" replace />;
 
   return children;
 };
 
 export default ProtectedRoute;
+
